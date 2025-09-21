f471b7a5d709166022ec61b726971f45ea51d78c
Michal Lustig <lmichal43@gmail.com>
2025-09-21 23:49:41 +0000

    task7.py

---
+++ homework1/src/task7.py
@@ -0,0 +1,6 @@
+import numpy as numpy
+
+#this function is meant to show the numpy package and some abilities we can do
+def numpy_array(array)
+
+    return {"this is the sum": numpy.sum(array), "this is the mean": numpy.mean(array), "this is the max": numpy.max(array),}
