e7a2c437281fcbad5e43eec5933622af210f52e2
Michal Lustig <lmichal43@gmail.com>
2025-10-06 07:09:57 +0000

    fixing errors before render

--- movie_theater_booking/settings.py
+++ movie_theater_booking/settings.py
@@ -142,6 +142,7 @@ STATIC_URL = 'static/'
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
